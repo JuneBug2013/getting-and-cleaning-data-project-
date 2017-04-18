bcea0a444a38a3881a1ffef97fb39feeddc95916
June Wang <jwang@Junes-MBP.hsd1.wa.comcast.net>
2017-04-18 14:25:35 -0700

    newly update

---
+++ CodeBook.md
@@ -0,0 +1,38 @@
+This code book explains the variables in the tidydata.txt file.
+
+Identifiers
+subject - The ID of the test subject
+activity - The type of activity performed when the corresponding measurements were taken
+
+Measurements
+ [1] "timeBodyAccelerometer-mean()-X"                 "timeBodyAccelerometer-mean()-Y"                 "timeBodyAccelerometer-mean()-Z"                
+ [4] "timeBodyAccelerometer-std()-X"                  "timeBodyAccelerometer-std()-Y"                  "timeBodyAccelerometer-std()-Z"                 
+ [7] "timeGravityAccelerometer-mean()-X"              "timeGravityAccelerometer-mean()-Y"              "timeGravityAccelerometer-mean()-Z"             
+[10] "timeGravityAccelerometer-std()-X"               "timeGravityAccelerometer-std()-Y"               "timeGravityAccelerometer-std()-Z"              
+[13] "timeBodyAccelerometerJerk-mean()-X"             "timeBodyAccelerometerJerk-mean()-Y"             "timeBodyAccelerometerJerk-mean()-Z"            
+[16] "timeBodyAccelerometerJerk-std()-X"              "timeBodyAccelerometerJerk-std()-Y"              "timeBodyAccelerometerJerk-std()-Z"             
+[19] "timeBodyGyroscope-mean()-X"                     "timeBodyGyroscope-mean()-Y"                     "timeBodyGyroscope-mean()-Z"                    
+[22] "timeBodyGyroscope-std()-X"                      "timeBodyGyroscope-std()-Y"                      "timeBodyGyroscope-std()-Z"                     
+[25] "timeBodyGyroscopeJerk-mean()-X"                 "timeBodyGyroscopeJerk-mean()-Y"                 "timeBodyGyroscopeJerk-mean()-Z"                
+[28] "timeBodyGyroscopeJerk-std()-X"                  "timeBodyGyroscopeJerk-std()-Y"                  "timeBodyGyroscopeJerk-std()-Z"                 
+[31] "timeBodyAccelerometerMagnitude-mean()"          "timeBodyAccelerometerMagnitude-std()"           "timeGravityAccelerometerMagnitude-mean()"      
+[34] "timeGravityAccelerometerMagnitude-std()"        "timeBodyAccelerometerJerkMagnitude-mean()"      "timeBodyAccelerometerJerkMagnitude-std()"      
+[37] "timeBodyGyroscopeMagnitude-mean()"              "timeBodyGyroscopeMagnitude-std()"               "timeBodyGyroscopeJerkMagnitude-mean()"         
+[40] "timeBodyGyroscopeJerkMagnitude-std()"           "frequencyBodyAccelerometer-mean()-X"            "frequencyBodyAccelerometer-mean()-Y"           
+[43] "frequencyBodyAccelerometer-mean()-Z"            "frequencyBodyAccelerometer-std()-X"             "frequencyBodyAccelerometer-std()-Y"            
+[46] "frequencyBodyAccelerometer-std()-Z"             "frequencyBodyAccelerometerJerk-mean()-X"        "frequencyBodyAccelerometerJerk-mean()-Y"       
+[49] "frequencyBodyAccelerometerJerk-mean()-Z"        "frequencyBodyAccelerometerJerk-std()-X"         "frequencyBodyAccelerometerJerk-std()-Y"        
+[52] "frequencyBodyAccelerometerJerk-std()-Z"         "frequencyBodyGyroscope-mean()-X"                "frequencyBodyGyroscope-mean()-Y"               
+[55] "frequencyBodyGyroscope-mean()-Z"                "frequencyBodyGyroscope-std()-X"                 "frequencyBodyGyroscope-std()-Y"                
+[58] "frequencyBodyGyroscope-std()-Z"                 "frequencyBodyAccelerometerMagnitude-mean()"     "frequencyBodyAccelerometerMagnitude-std()"     
+[61] "frequencyBodyAccelerometerJerkMagnitude-mean()" "frequencyBodyAccelerometerJerkMagnitude-std()"  "frequencyBodyGyroscopeMagnitude-mean()"        
+[64] "frequencyBodyGyroscopeMagnitude-std()"          "frequencyBodyGyroscopeJerkMagnitude-mean()"     "frequencyBodyGyroscopeJerkMagnitude-std()"     
+[67] "subject"  "activity"   
+
+Activity Labels
+WALKING: subject was walking during the test
+WALKING_UPSTAIRS: subject was walking up a staircase during the test
+WALKING_DOWNSTAIRS: subject was walking down a staircase during the test
+SITTING: subject was sitting during the test
+STANDING: subject was standing during the test
+LAYING: subject was laying down during the test
--- run_analysis.R
+++ run_analysis.R
@@ -13,70 +13,71 @@ download.file(fileUrl,destfile="./week4/Dataset.zip")
 
 # Unzip dataSet to /data directory
 unzip(zipfile="./week4/Dataset.zip",exdir="./week4")
-x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 
 ### Merges the training and the test sets to create one data set
 # read the training tables
-x_train <- read.table("./week4/UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./week4/UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("./week4/UCI HAR Dataset/train/subject_train.txt")
+x_train <- read.table("./week4/UCI HAR Dataset/train/X_train.txt",header=FALSE)
+y_train <- read.table("./week4/UCI HAR Dataset/train/y_train.txt",header=FALSE)
+subject_train <- read.table("./week4/UCI HAR Dataset/train/subject_train.txt",header=FALSE)
 
 # read the test tables
-x_test <- read.table("./week4/UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./week4/UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./week4/UCI HAR Dataset/test/subject_test.txt")
+x_test <- read.table("./week4/UCI HAR Dataset/test/X_test.txt",header=FALSE)
+y_test <- read.table("./week4/UCI HAR Dataset/test/y_test.txt",header=FALSE)
+subject_test <- read.table("./week4/UCI HAR Dataset/test/subject_test.txt",header=FALSE)
 
 # read feature vector 
-features <- read.table('./week4/UCI HAR Dataset/features.txt')
+featuresname <- read.table('./week4/UCI HAR Dataset/features.txt',header=FALSE)
 
 # concatenate the data tables by row
 subject <- rbind (subject_train, subject_test)
 activity <- rbind (y_train,y_test) 
 features <- rbind (x_train, x_test)
+
+str(subject)
+str(activity)
+str(features)
+
 # add column names 
 names(subject) <- c("subject")
 names(activity) <- c("activity")
-names(features) <- dataFeaturesNames$V2
+names(features) <- featuresname$V2
 
 # merge all data in one set
 merge <- cbind(subject, activity)
 data <- cbind(features, merge)
-
+ 
 ### Extracts only the measurements on the mean and standard deviation for each measurement.
 ## taken Names of Features with “mean()” or “std()”
-subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)] 
-selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
-data <- subset(data,select=selectedNames)
-str(data)
+subdataFeaturesNames <- featuresname$V2[grep("mean\\(\\)|std\\(\\)", featuresname$V2)] 
+subdataFeaturesNames 
+selectedNames <- c(as.character(subdataFeaturesNames), "subject", "activity" )
+selectedNames
+newdata <-subset(data,  select = selectedNames)
+   
+ 
 
 ### Uses descriptive activity names to name the activities in the data set.
 # read activity labels
 activityLabels = read.table('./week4/UCI HAR Dataset/activity_labels.txt')
 ## facorize Variale activity in the data frame Data using descriptive activity names
 # turn activities & subjects into factors
-data$activity <- factor(data$activity, levels = activityLabels[,1], labels = activityLabels[,2])
-data$subject <- as.factor(data$subject)
-head(data$activity,50)
+newdata$activity <- factor(newdata$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+newdata$subject <- as.factor(newdata$subject)
+head(newdata$activity,50)
 
 ### Appropriately labels the data set with descriptive variable names.
-names(data)<-gsub("^t", "time", names(data))
-names(data)<-gsub("^f", "frequency", names(data))
-names(data)<-gsub("Acc", "Accelerometer", names(data))
-names(data)<-gsub("Gyro", "Gyroscope", names(data))
-names(data)<-gsub("Mag", "Magnitude", names(data))
-names(data)<-gsub("BodyBody", "Body", names(data))
-head(data)
-
+names(newdata)<-gsub("^t", "time", names(newdata))
+names(newdata)<-gsub("^f", "frequency", names(newdata))
+names(newdata)<-gsub("Acc", "Accelerometer", names(newdata))
+names(newdata)<-gsub("Gyro", "Gyroscope", names(newdata))
+names(newdata)<-gsub("Mag", "Magnitude", names(newdata))
+names(newdata)<-gsub("BodyBody", "Body", names(newdata))
+head(newdata)
+names(newdata)
 ### creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 # make second tidy data set
-tidydata2 <- aggregate(. ~ subject + activity, data, mean)
-tidydata2 <- tidydata2[order(tidydata2$subject, tidydata2$activity),]
+tidydata <- aggregate(. ~ subject + activity, newdata, mean)
+tidydata <- tidydata[order(tidydata$subject, tidydata$activity),]
 # write second tidy data in txt file 
-write.table(tidydata2,"tidydata2.txt",row.names = FALSE)
-
-
-
-
-
-
+write.table(tidydata,"tidydata.txt",row.names = FALSE)
 
